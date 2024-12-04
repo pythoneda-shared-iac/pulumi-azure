b768375ff2058de9117025f56a9fc3d17085feac
rydnr <github@acm-sl.org>
2024-12-04 09:01:15 +0100

    Fixed imports

--- pythoneda/shared/iac/pulumi/azure/function_storage_account.py
+++ pythoneda/shared/iac/pulumi/azure/function_storage_account.py
@@ -19,8 +19,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from pythoneda.iac.pulumi.azure import ResourceGroup, StorageAccount
 import pulumi
+from .resource_group import ResourceGroup
+from .storage_account import StorageAccount
 
 
 class FunctionStorageAccount(StorageAccount):
